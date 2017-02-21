e86cca3c18953b050c56274ccbdb24671beab088
ThiagoLuizNunes <thiago.luiz@lavid.ufpb.br>
2017-02-21 09:08:58 -0300

    First challenge JavaScript Ninja course

---
+++ challenge-01.js
@@ -0,0 +1,23 @@
+var myvar;
+myvar = 10;
+console.log(myvar);
+var soma = 15+8;
+soma+=1;
+console.log(soma);
+soma*=3;
+console.log(soma);
+
+var souninja = true;
+var comida = ['arroz', 'feijão', 'ovo'];
+console.log(comida[1]);
+
+if(soma !== myvar){
+  console.log(true);
+}
+if(myvar <= soma){
+  console.log(true);
+}
+function divisao(num1, num2) {
+  return num1/num2;
+}
+console.log(divisao(10,2));
